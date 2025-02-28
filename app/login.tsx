ea02477cbf47015d605611c3b0d79f0219e397cc
Massy Haddad <massy.haddad.1@ens.etsmtl.ca>
2025-02-28 15:26:24 -0500

    Login screen, themed text input

--- app/login.tsx
+++ app/login.tsx
@@ -1,23 +1,21 @@
 import { useState } from 'react'
 import { router } from 'expo-router'
+import { FontAwesome } from '@expo/vector-icons'
+import { View, Text, Image, TouchableOpacity } from 'react-native'
 import { BlurView } from 'expo-blur'
-import { Feather, FontAwesome } from '@expo/vector-icons'
-import {
-	View,
-	Text,
-	TextInput,
-	TouchableOpacity,
-	ImageBackground,
-	Image,
-} from 'react-native'
+import Checkbox from 'expo-checkbox'
 
 import '../global.css'
 import { useSession } from './ctx'
 import { ThemedText } from '@/components/ThemedText'
+import { ThemedView } from '@/components/ThemedView'
+import { useColorScheme } from '@/hooks/useColorScheme.web'
+import { ThemedTextInput } from '@/components/ThemedTextInput'
 
 export default function LoginScreen() {
 	const [email, setEmail] = useState('')
 	const [password, setPassword] = useState('')
+	const [isChecked, setChecked] = useState(false)
 
 	const { signIn } = useSession()
 
@@ -26,11 +24,10 @@ export default function LoginScreen() {
 		router.replace('/')
 	}
 
+	const colorScheme = useColorScheme()
+
 	return (
-		<ImageBackground
-			source={require('@/assets/images/bg2.jpg')}
-			className="flex-1 justify-end items-center"
-		>
+		<ThemedView className="flex-1 justify-end items-center">
 			<View className="absolute top-1/4 -left-56 -mt-40">
 				<Image
 					source={require('@/assets/images/flower.png')}
@@ -50,45 +47,44 @@ export default function LoginScreen() {
 				LOGIN TO{'\n'}YOUR ACCOUNT
 			</Text>
 			<BlurView
-				style={{ backgroundColor: 'rgba(0,0,0,0.90)', overflow: 'hidden' }}
-				tint="dark"
+				style={
+					colorScheme === 'dark'
+						? { backgroundColor: 'rgba(0,0,0,0.90)', overflow: 'hidden' }
+						: { backgroundColor: 'rgba(255,255,255,0.90)', overflow: 'hidden' }
+				}
+				tint={colorScheme === 'dark' ? 'dark' : 'light'}
 				intensity={20}
 				className="flex h-3/4 w-full p-6 pt-0 justify-start items-center border-t-[1px] border-gray-50/15"
 			>
-				<ThemedText type="default" className="text-gray-300 my-12 text-center">
+				<ThemedText type="default" className="my-12 text-center">
 					Enter your login information
 				</ThemedText>
 
-				<View className="mb-4 flex-row items-center bg-white/10 p-2 rounded-xl border border-white/20 w-full">
-					<Feather name="mail" size={20} color="white" className="mx-4" />
-					<TextInput
-						style={{ fontFamily: 'Lexend' }}
-						placeholder="Email"
-						placeholderTextColor="#ddd"
-						className="text-white flex-1 font-light"
-						value={email}
-						onChangeText={setEmail}
-					/>
-				</View>
+				<ThemedTextInput
+					icon="user"
+					placeholder="Email"
+					value={email}
+					onChangeText={setEmail}
+				/>
 
-				<View className="mb-4 flex-row items-center bg-white/10 p-2 rounded-xl border border-white/20 w-full">
-					<Feather name="lock" size={20} color="white" className="mx-4" />
-					<TextInput
-						style={{ fontFamily: 'Lexend' }}
-						placeholder="Password"
-						placeholderTextColor="#ddd"
-						secureTextEntry
-						className="text-white flex-1 font-light"
-						value={password}
-						onChangeText={setPassword}
-					/>
-					<Feather name="eye-off" size={20} color="white" className="mx-2" />
-				</View>
+				<ThemedTextInput
+					icon="lock"
+					placeholder="Password"
+					secureTextEntry
+					value={password}
+					onChangeText={setPassword}
+				/>
 
 				<View className="flex-row justify-between items-center mb-8 w-full">
-					<TouchableOpacity>
+					<View className="flex-row justify-between items-center gap-2">
+						<Checkbox
+							value={isChecked}
+							onValueChange={setChecked}
+							style={{ width: 16, height: 16 }}
+							color={colorScheme === 'dark' ? 'white' : 'black'}
+						/>
 						<ThemedText className="text-gray-300">Remember me</ThemedText>
-					</TouchableOpacity>
+					</View>
 					<TouchableOpacity>
 						<ThemedText className="text-blue-400">Forgot password</ThemedText>
 					</TouchableOpacity>
@@ -96,7 +92,7 @@ export default function LoginScreen() {
 
 				<TouchableOpacity
 					onPress={() => handleLogin()}
-					className="bg-blue-500 p-3 rounded-xl w-full flex-row justify-center items-center mb-4"
+					className="bg-blue-500 p-4 rounded-xl w-full flex-row justify-center items-center mb-4"
 				>
 					<ThemedText type="subtitle" className="text-white font-semibold">
 						LOGIN
@@ -110,7 +106,7 @@ export default function LoginScreen() {
 				</View>
 
 				<View className="flex-row justify-between w-full mb-4">
-					<TouchableOpacity className="bg-white/20 p-3 rounded-xl flex-row items-center justify-center w-1/2 mr-2">
+					<TouchableOpacity className="bg-white/20 p-4 rounded-xl flex-row items-center justify-center w-1/2 mr-2">
 						<FontAwesome
 							name="google"
 							size={24}
@@ -130,11 +126,13 @@ export default function LoginScreen() {
 					</TouchableOpacity>
 				</View>
 
-				<ThemedText className="text-gray-300 text-sm text-center mt-8">
-					Don't have an account?{' '}
-					<ThemedText className="text-blue-400">Sign Up</ThemedText>
-				</ThemedText>
+				<TouchableOpacity onPress={() => router.push('/')}>
+					<ThemedText className="flex-1 justify-center items-center text-gray-300 text-sm text-center mt-8">
+						Don't have an account?{'  '}
+						<ThemedText type="link">Sign Up</ThemedText>
+					</ThemedText>
+				</TouchableOpacity>
 			</BlurView>
-		</ImageBackground>
+		</ThemedView>
 	)
 }
