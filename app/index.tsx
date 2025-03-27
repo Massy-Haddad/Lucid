eb7a062afad355edefd93ab450a08f33417e393a
Massy Haddad <massy.haddad@lepisc.com>
2025-03-26 21:04:27 -0400

    eas build ready for production

--- app/index.tsx
+++ app/index.tsx
@@ -12,36 +12,27 @@ const Welcome = () => {
 	const { session, isLoading } = useSession()
 
 	if (!isLoading && session) return <Redirect href="/(tabs)/(home)/animes" />
+	else if (!isLoading && !session) return <Redirect href="/auth?mode=signin" />
 
 	return (
 		<>
 			<ImagePreloader />
 			<SafeAreaView className="bg-primary h-full">
 				{isLoading && <ActivityIndicator size="large" color="#0000ff" />}
+				<View className="w-full flex justify-center items-center h-full px-4">
+					<Image
+						source={require('@/assets/images/icons/splash-icon-light.png')}
+						className="max-w-[200px] w-full h-[200px]"
+						resizeMode="contain"
+					/>
 
-				<ScrollView
-					contentContainerStyle={{
-						height: '100%',
-					}}
-				>
-					<View className="w-full flex justify-center items-center h-full px-4">
-						<Image
-							source={require('@/assets/images/icons/splash-icon-light.png')}
-							className="max-w-[200px] w-full h-[200px]"
-							resizeMode="contain"
-						/>
-
-						<View
-							className="w-1/2 mt-7 p-4 rounded-lg bg-purple-100/20"
-							onTouchEnd={() => router.push('/auth?mode=signin')}
-						>
-							<ThemedText className="text-center">
-								Continue with Email
-							</ThemedText>
-						</View>
+					<View
+						className="w-1/2 mt-7 p-4 rounded-lg bg-purple-100/20"
+						onTouchEnd={() => router.push('/auth?mode=signin')}
+					>
+						<ThemedText className="text-center">Continue with Email</ThemedText>
 					</View>
-				</ScrollView>
-
+				</View>
 				<StatusBar backgroundColor="#161622" style="light" />
 			</SafeAreaView>
 		</>
